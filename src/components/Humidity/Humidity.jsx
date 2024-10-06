1125370a45aeb9dfb6cf0058b91509c55778314a
enndesigner <anna20080322@gmail.com>
2024-10-06 22:39:38 +0300

    yeah

--- src/components/Humidity/Humidity.jsx
+++ src/components/Humidity/Humidity.jsx
@@ -17,14 +17,14 @@ const Humidity = () => {
                         <div className="ListData">22/09/01</div>
                     </div>
                 </div>
-                <span style={{fontSize: '96px', fontFamily: '"Orbitron", sans-serif'}}>70%</span>
-                <p style={{width: '400px'}}>*When humidity is over 65% it means it is on its the highest level. At this time lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget vestibulum neque, id venenatis lorem. Aenean faucibus nibh vitae tempus cursus.read about humadity on tips page</p>
+                <div style={{display: 'flex'}}><span style={{fontSize: '96px', marginRight: '30px', fontFamily: '"Orbitron", sans-serif'}}>70%</span>
+                <p style={{width: '400px'}}>*When humidity is over 65% it means it is on its the highest level. At this time lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget vestibulum neque, id venenatis lorem. Aenean faucibus nibh vitae tempus cursus.read about humadity on tips page</p></div>
                 <p>75% Relative Humidity<br/>
-Dew Point: 17°C<br/>
-Absolute Humidity: 12.5 g/m³<br/>
-Humidity Ratio: 9.5 g/kg<br/>
-Humidity Index: 22.0°C<br/>
-Pressure: 15.5 hPa</p>
+                    Dew Point: 17°C<br/>
+                    Absolute Humidity: 12.5 g/m³<br/>
+                    Humidity Ratio: 9.5 g/kg<br/>
+                    Humidity Index: 22.0°C<br/>
+                    Pressure: 15.5 hPa</p>
                 <WeatherApp/>
         </div>
     );
